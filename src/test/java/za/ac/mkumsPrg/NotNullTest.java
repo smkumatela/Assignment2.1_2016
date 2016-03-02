9216b5b8a3163eb9740e7c8a975a64bb8144e015
smkumatela <smkumatela@webmail.co.za>
2016-03-02 16:51:29 +0200

    Classes only

--- src/test/java/za/ac/mkumsPrg/NotNullTest.java
+++ src/test/java/za/ac/mkumsPrg/NotNullTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
 
 /**
  * Created by Songezo on 2016-03-01.
@@ -21,6 +24,12 @@ public class NotNullTest {
     @After
     public void tearDown() throws Exception {
 
+
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return null;
 
     }
 
